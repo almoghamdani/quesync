0c2124474e2c287e8c5acd0ad3ee853b18aa4808
Almog Hamdani <almogh52@gmail.com>
2018-12-21 23:54:44 +0200

    Fix wrong file name in the server build

--- runClient.bat
+++ runClient.bat
@@ -4,4 +4,8 @@ IF NOT EXIST Build mkdir Build
 :: Re-compile the client side
 del Build\Client.exe
 g++ client.cpp -L lib -lbass -lopus -lws2_32 -o Build\Client.exe
+
+del Build\Server.exe
+g++ server.cpp -L lib -lopus -lws2_32 -o Build\Server.exe
+
 Build\Client.exe
