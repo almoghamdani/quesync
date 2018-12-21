ca0c0dfeb6c2dd38f3ebe5c3ebc0044a08855260
Almog Hamdani <almogh52@gmail.com>
2018-12-21 21:35:11 +0200

    Add check for incompatible BASS version

--- client.cpp
+++ client.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-    std::cout << BASS_GetVersion() << std::endl;
+    // Check if the correct version of the BASS library has been loaded
+    if (HIWORD(BASS_GetVersion()) != BASSVERSION)
+    {
+        std::cout << "Incorrect version of BASS library has been loaded! Exiting.." << std::endl;
+    }
+
+    // Print BASS version
+    std::cout << "BASS library version: " << BASS_GetVersion() << std::endl;
 
     return 0;
 }
