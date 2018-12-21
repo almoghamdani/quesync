f92c97fe9dfdadc3b45d491fa2a6282950a8c60b
Almog Hamdani <almogh52@gmail.com>
2018-12-21 23:54:32 +0200

    Add build for the server side

--- runClient.bat
+++ runClient.bat
@@ -1,3 +1,7 @@
+::Create the Build folder if it doesn't exist
 IF NOT EXIST Build mkdir Build
+
+:: Re-compile the client side
+del Build\Client.exe
 g++ client.cpp -L lib -lbass -lopus -lws2_32 -o Build\Client.exe
 Build\Client.exe
