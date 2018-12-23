5e15ec5d20fda9b6ac6b27865630e150613320e6
Almog Hamdani <almogh52@gmail.com>
2018-12-23 20:51:02 +0200

    Change all build folder names and it's files to lowercase

--- build.bat
+++ build.bat
@@ -1,14 +1,14 @@
 ::Re-create the build folder
-IF EXIST Build rmdir BUILD /s /q
-mkdir BUILD
+IF EXIST build rmdir build /s /q
+mkdir build
 
 :: Copy all needed dlls to build folder
-xcopy /s dlls Build
+xcopy /s dlls build
 
 :: Re-compile the client side
-IF EXIST Build\Client.exe del Build\Client.exe
-g++ client.cpp -L lib -lbass -lopus -lOpenAL32 -lws2_32 -o Build\Client.exe
+IF EXIST build\client.exe del build\client.exe
+g++ client.cpp -L lib -lbass -lopus -lOpenAL32 -lws2_32 -o build\client.exe
 
 :: Re-compile the server side
-IF EXIST Build\Server.exe del Build\Server.exe
-g++ server.cpp -L lib -lopus -lbass -lws2_32 -o Build\Server.exe
+IF EXIST build\server.exe del build\server.exe
+g++ server.cpp -L lib -lopus -lbass -lws2_32 -o build\server.exe
