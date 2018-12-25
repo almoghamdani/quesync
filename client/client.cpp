0046cfa70c19a20cdfb1423f61201b5a46dfec46
Almog Hamdani <almogh52@gmail.com>
2018-12-25 13:37:40 +0200

    Remove creating another socket for receiving by using a non-blocking socket for receiving and sending

--- client/client.cpp
+++ client/client.cpp
@@ -24,9 +24,9 @@
 using std::cout;
 using std::endl;
 
-void receivePCMInfo()
+void receivePCMInfo(SOCKET recvSocket)
 {
-    SOCKET recvSocket = INVALID_SOCKET;
+    //SOCKET recvSocket = INVALID_SOCKET;
     unsigned char buffer[5000] = {0};
     opus_int16 pcm[12000] = {0};
     int recvLen;
@@ -34,62 +34,6 @@ void receivePCMInfo()
     int decodedSize = 0;
     HSTREAM stream;
     int socketError;
-    struct addrinfo *result = NULL,
-                    *ptr = NULL,
-                    hints;
-    struct sockaddr_in sin;
-    int sinAddrLen = sizeof(sin);
-    std::string localPortStr = "THIS_IS_PORT_MESSAGE";
-
-    // Zero the memory of the hints address and set the connection type as UDP
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_protocol = IPPROTO_UDP;
-
-    // Resolve the server address and port
-    if ((socketError = getaddrinfo("127.0.0.1", "55556", &hints, &result)) != 0) {
-        cout << "Failed to get address info, Error Code: " << socketError << ". Exiting.." << endl;
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-    cout << "Converted Server IP and Port successfully!" << endl;
-
-    // The address should be the first item returned to the result
-    ptr=result;
-
-    // Create a SOCKET for connecting to server
-    recvSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-    if (recvSocket == INVALID_SOCKET) {
-        cout << "Error at creating a socket, Error code: " << WSAGetLastError() << ". Exiting.." << endl;
-        freeaddrinfo(result);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-    cout << "Created socket successfully!" << endl;
-    cout << "Connecting to server..." << endl;
-
-    // Connect to server.
-    if ((socketError = connect(recvSocket, ptr->ai_addr, (int)ptr->ai_addrlen)) == SOCKET_ERROR) {
-        // If failed close socket
-        closesocket(recvSocket);
-        recvSocket = INVALID_SOCKET;
-    }
-
-    // Clear the address info for the connection details and check if there was an error
-    freeaddrinfo(result);
-    if (recvSocket == INVALID_SOCKET) {
-        cout << "Unable to connect to server, Error code: " << socketError << ". Exiting.." << endl;
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-    cout << "Socket connected to server!" << endl;
-
-    // Try to send the data
-    if (send(recvSocket, localPortStr.c_str(), strlen(localPortStr.c_str()), 0) == SOCKET_ERROR)
-    {
-        cout << "Send to server failed, Error code: " << WSAGetLastError() << ". Skipping.." << endl;
-    }
 
     // Create the opus decoder for the recording
     opusDecoder = opus_decoder_create(RECORD_FREQUENCY, RECORD_CHANNELS, NULL);
@@ -107,22 +51,18 @@ void receivePCMInfo()
 
     while (true)
     {
-        // Get data from client
-        if ((recvLen = recv(recvSocket, (char *)buffer, 5000, 0)) == SOCKET_ERROR)
+        // Get data from client, check if the buffer isn't empty by getting the winsock error
+        if ((recvLen = recv(recvSocket, (char *)buffer, 5000, 0)) == SOCKET_ERROR && ((socketError = WSAGetLastError()) != WSAEWOULDBLOCK))
         {
             std::cout << "Failed to receive data, Error code: " << WSAGetLastError() << ". Skipping.." << std::endl;
             continue;
         }
 
-        cout << "Received data length: " << recvLen << endl;
-
         // If the buffer isn't empty, decode the pcm info and put it in the stream
-        if (recvLen != 0)
+        if (recvLen != 0 && socketError != WSAGetLastError())
         {
             // Decode the current sample from the client
             decodedSize = opus_decode(opusDecoder, buffer, recvLen, (opus_int16 *)pcm, 960, 0);
-
-            cout << "Encoded data samples: " << decodedSize << endl;
 
             // Put the decoded pcm data in the stream
             BASS_StreamPutData(stream, pcm, decodedSize * sizeof(opus_int16) * 2);
@@ -146,6 +86,7 @@ int main()
     OpusEncoder *opusEncoder;
     int dataLen = 0;
     std::thread recvThread;
+    u_long iMode = 1;
 
     // Print Opus library version
     cout << "Opus library version: " << opus_get_version_string() << endl;
@@ -165,7 +106,7 @@ int main()
     hints.ai_protocol = IPPROTO_UDP;
 
     // Resolve the server address and port
-    if ((socketError = getaddrinfo("127.0.0.1", "55556", &hints, &result)) != 0) {
+    if ((socketError = getaddrinfo("109.67.245.4", "55556", &hints, &result)) != 0) {
         cout << "Failed to get address info, Error Code: " << socketError << ". Exiting.." << endl;
         WSACleanup();
         exit(EXIT_FAILURE);
@@ -202,8 +143,17 @@ int main()
     }
     cout << "Socket connected to server!" << endl;
 
+    // Try to make the socket non-blocking
+    if((socketError = ioctlsocket(ConnectSocket, FIONBIO, &iMode)) != NO_ERROR)
+    {
+        cout << "Unable to make socket non-blocking, Error code: " << socketError << ". Exiting.." << endl;
+        closesocket(ConnectSocket);
+        WSACleanup();
+        exit(EXIT_FAILURE);
+    }
+
     // Start the recv thread
-    recvThread = std::thread(receivePCMInfo);
+    recvThread = std::thread(receivePCMInfo, ConnectSocket);
     recvThread.detach();
 
     // Create the opus encoder for the recording
@@ -225,6 +175,7 @@ int main()
         // Get the amount of samples waiting in the device's buffer
         alcGetIntegerv(captureDevice, ALC_CAPTURE_SAMPLES, (ALCsizei)sizeof(ALint), &sample);
 
+        // If there is enough samples to send the server to match the server's framerate, send it
         if (sample >= FRAMERATE)
         {
             // Get the capture samples
@@ -232,8 +183,6 @@ int main()
             
             // Encode the captured data
             dataLen = opus_encode(opusEncoder, (const opus_int16 *)buffer, FRAMERATE, encodedBuffer, FRAMERATE * RECORD_CHANNELS * sizeof(opus_int16));
-
-            //cout << "Sent data length: " << dataLen << endl;
 
             // If the socket is valid, send the encoded data through it
             if (ConnectSocket != INVALID_SOCKET)
