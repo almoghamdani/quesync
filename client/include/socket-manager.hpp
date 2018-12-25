3461fae2e78de1dd9a5df569b3881d628bf78512
Almog Hamdani <almogh52@gmail.com>
2018-12-25 15:09:15 +0200

    Change socket manager errors to exceptions

--- client/include/socket-manager.hpp
+++ client/include/socket-manager.hpp
@@ -7,6 +7,8 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
+#include "socket-error.hpp"
+
 class SocketManager
 {
 public:
--- client/socket-manager.cpp
+++ client/socket-manager.cpp
@@ -11,11 +11,9 @@ void SocketManager::initWinsock()
     WSADATA wsaData;
 
     cout << "Initializing Winsock..." << endl;
-    //* Initialize Winsock
+    //* Initialize Winsock, if falied throw startup failure exception
     if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
-        // Print error code
-        cout << "WSAStartup failed, Error code: " << WSAGetLastError() << ". Exiting.." << endl;
-        exit(EXIT_FAILURE);
+        throw SocketError("WSAStartup failed", WSAGetLastError());
     }
     cout << "Winsock Initialized!" << endl;
 }
@@ -34,21 +32,17 @@ SOCKET SocketManager::createUDPSocket(const char *ipAddress, const char *port, b
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_protocol = IPPROTO_UDP;
 
-    // Resolve the server address and port
+    // Resolve the server address and port, if failed throw exception
     if ((socketError = getaddrinfo(ipAddress, port, &hints, &result)) != 0) {
-        cout << "Failed to get address info, Error Code: " << socketError << ". Exiting.." << endl;
-        WSACleanup();
-        exit(EXIT_FAILURE);
+        throw SocketError("Failed to get address info", WSAGetLastError());
     }
     cout << "Converted Server IP and Port successfully!" << endl;
 
     // Create a SOCKET for connecting to server
     udpSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     if (udpSocket == INVALID_SOCKET) {
-        cout << "Error at creating a socket, Error code: " << WSAGetLastError() << ". Exiting.." << endl;
-        freeaddrinfo(result);
-        WSACleanup();
-        exit(EXIT_FAILURE);
+        freeaddrinfo(result); // Free the address info
+        throw SocketError("Error at creating a socket", WSAGetLastError());
     }
     cout << "Created socket successfully!" << endl;
     cout << "Connecting to server..." << endl;
@@ -57,17 +51,13 @@ SOCKET SocketManager::createUDPSocket(const char *ipAddress, const char *port, b
     if ((socketError = connect(udpSocket, result->ai_addr, (int)result->ai_addrlen)) == SOCKET_ERROR) {
         // If failed close socket and set it as invalid socket
         closesocket(udpSocket);
-        udpSocket = INVALID_SOCKET;
-    }
-
-    // Clear the address info for the connection details and check if there was an error
-    freeaddrinfo(result);
-    if (udpSocket == INVALID_SOCKET) {
-        cout << "Unable to connect to server, Error code: " << socketError << ". Exiting.." << endl;
-        WSACleanup();
-        exit(EXIT_FAILURE);
+        freeaddrinfo(result);
+        throw SocketError("Unable to connect to server", socketError);
     }
     cout << "Socket connected to server!" << endl;
+
+    // Free the server's address info since it's not needed anymore
+    freeaddrinfo(result);
 
     // If the user asked to create a non-blocking socket
     if (nonBlocking)
@@ -75,10 +65,8 @@ SOCKET SocketManager::createUDPSocket(const char *ipAddress, const char *port, b
         // Try to make the socket non-blocking
         if((socketError = ioctlsocket(udpSocket, FIONBIO, &nonBlockingMode)) != NO_ERROR)
         {
-            cout << "Unable to make socket non-blocking, Error code: " << socketError << ". Exiting.." << endl;
             closesocket(udpSocket);
-            WSACleanup();
-            exit(EXIT_FAILURE);
+            throw SocketError("Unable to make socket non-blocking", socketError);
         }
     }
 
