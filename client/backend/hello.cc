40ee05083b607ccd4e4f859fe5f0b4946515e0a4
Almog Hamdani <almogh52@gmail.com>
2018-12-27 21:03:23 +0200

    Trying to use libuv sockets

--- client/backend/hello.cc
+++ client/backend/hello.cc
@@ -4,16 +4,67 @@
 #include "include/socket-manager.hpp"
 #include "include/voice-chat.hpp"
 
+void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) 
+{
+    buf->base = (char *)malloc(suggested_size);
+    buf->len = suggested_size;
+}
+
+void on_read(uv_udp_t* handle, ssize_t nread, const uv_buf_t* buf, const struct sockaddr* addr, unsigned flags) {
+  std::cout << "Yovel tembel" << std::endl;
+    
+  if (nread == -1) {
+    fprintf(stderr, "Read error!\n");
+    uv_close((uv_handle_t*) handle, NULL);
+    free(buf->base);
+    return;
+  }
+
+  char sender[17] = { 0 };
+  uv_ip4_name((struct sockaddr_in*) addr, sender, 16);
+  fprintf(stderr, "Recv from %s\n", sender);
+}
+
+void on_send(uv_udp_send_t *req, int status)
+{
+  std::cout << "Sima" << std::endl;
+}
+
 namespace demo {
 
 napi_value Method(napi_env env, napi_callback_info args) {
   napi_value greeting;
   napi_status status;
   VoiceChat *voiceChatManager;
+  uv_loop_t *loop;
+  uv_udp_t socket;
 
-  SocketManager::initWinsock();
+  //SocketManager::initWinsock();
 
-  voiceChatManager = new VoiceChat("127.0.0.1");
+  //voiceChatManager = new VoiceChat("127.0.0.1");
+
+  std::cout << napi_get_uv_event_loop(env, &loop) << std::endl;
+
+  uv_udp_init(loop, &socket);
+
+  uv_udp_send_t send_req;
+  uv_buf_t buffer; 
+  alloc_buffer((uv_handle_t*)&send_req, 256, &buffer);
+  memset(buffer.base, 0, buffer.len);
+
+  struct sockaddr_in addr;
+  uv_ip4_addr("0.0.0.0", 0, (sockaddr_in *)&addr);
+  uv_udp_bind(&socket, (const struct sockaddr *)&addr, 0);
+
+  std::cout << "Hello" << std::endl;
+
+  struct sockaddr_in send_addr;
+  uv_ip4_addr("127.0.0.1", 61111, (sockaddr_in *)&send_addr);
+  uv_udp_send(&send_req, &socket, &buffer, 1, (const sockaddr *)&send_addr, (uv_udp_send_cb)on_send);
+
+  std::cout << uv_udp_recv_start(&socket, alloc_buffer, on_read) << std::endl;
+
+  uv_run(loop, UV_RUN_DEFAULT);
 
   status = napi_create_string_utf8(env, "hello", NAPI_AUTO_LENGTH, &greeting);
   if (status != napi_ok) return nullptr;
