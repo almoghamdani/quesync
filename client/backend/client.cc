07add0f076c71252e62744aef3ae7e1a4957ede6
Almog Hamdani <almogh52@gmail.com>
2018-12-31 17:41:35 +0200

    Fix Client's NAN constructor + Remove unneeded variable

--- client/backend/client.cc
+++ client/backend/client.cc
@@ -3,8 +3,6 @@
 Nan::Persistent<v8::Function> Client::constructor;
 
 void Client::Init(v8::Local<v8::Object> exports) {
-  Nan::HandleScope scope;
-
   // Prepare constructor template
   v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
   tpl->SetClassName(Nan::New("Client").ToLocalChecked());
@@ -18,15 +16,13 @@ void Client::Init(v8::Local<v8::Object> exports) {
 
 void Client::New(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info.IsConstructCall()) {
-    // Invoked as constructor: `new MyObject(...)`
+    // Invoked as constructor: `new Client(...)`
     Client* obj = new Client();
     obj->Wrap(info.This());
     info.GetReturnValue().Set(info.This());
   } else {
-    // Invoked as plain function `MyObject(...)`, turn into construct call.
-    const int argc = 1;
-    v8::Local<v8::Value> argv[argc] = { info[0] };
+    // Return the object's constructor function
     v8::Local<v8::Function> cons = Nan::New<v8::Function>(constructor);
-    info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
+    info.GetReturnValue().Set(Nan::NewInstance(cons).ToLocalChecked());
   }
 }
