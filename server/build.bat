68930a9151ead3c192025fff7ac16a77a01d3014
Almog Hamdani <almogh52@gmail.com>
2019-01-02 00:30:20 +0200

    Move build batch script to the server and remove the old server code

---
+++ server/build.bat
@@ -0,0 +1,19 @@
+SET ARCH=x64
+
+::Re-create the build folder
+IF EXIST build rmdir build /s /q
+mkdir build
+
+:: Copy all needed dlls to build folder
+xcopy /s dlls build
+
+:: Initialize the visual studio developer command line with x64 if it's not initialized yet
+IF NOT DEFINED DevEnvDir (
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
+)
+
+SET LIB=%~dp0\..\lib;%LIB%
+
+:: Re-compile the server side
+IF EXIST build\server.exe del build\server.exe
+cl server.cpp -I..\include /EHsc /Fe"build\server.exe"
