d7db0f5f441e53578fdbeb0e6e35d3d2ece814ef
Almog Hamdani <almogh52@gmail.com>
2019-01-03 22:57:58 +0200

    Fix an issue with creating the quesync object

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 quesync = require("./build/Release/quesync.node")
 
-smth = quesync.Client()
+var smth = new quesync.Client()
 smth.connect("127.0.0.1");
-console.log(smth)
+//console.log(smth)
 
-while (true)
+function idk(hex)
 {
-    
+
 }
+
+setInterval(idk, 10, smth)
