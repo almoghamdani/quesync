393bdd63261901e810eb69f609fa381c88c192ff
Student <Student@MEKIF11.LOCAL>
2019-01-03 14:02:16 +0200

    Remove unneeded old module code

--- client/backend/hello.cc
+++ client/backend/hello.cc
@@ -1,20 +1,7 @@
 #include <nan.h>
-#include "include/socket-manager.hpp"
-#include "include/voice-chat.hpp"
 #include "include/client.hpp"
 
-/*void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  VoiceChat *voiceChatManager;
-
-  SocketManager::InitSocketManager();
-  voiceChatManager = new VoiceChat("127.0.0.1");
-
-  info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
-}*/
-
 void Init(v8::Local<v8::Object> exports) {
-  /*exports->Set(Nan::New("hello").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Method)->GetFunction());*/
   Client::Init(exports);
 }
 
