4bcda752da64b0626cbc4bcd72edac147c37e877
Student <Student@MEKIF11.LOCAL>
2019-01-04 13:02:51 +0200

    Remove app template + Add simple email textfeld

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonIcon } from '@rmwc/button';
-import 'material-components-web/dist/material-components-web.min.css'
+import { TextField } from '@rmwc/textfield';
+import 'material-components-web/dist/material-components-web.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button>sdsd</Button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TextField outlined label="E-Mail" />
       </div>
     );
   }
