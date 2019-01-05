99942beb89d4d4d60c5761e8e78b9eee2ef1259c
Almog Hamdani <almogh52@gmail.com>
2019-01-05 12:00:48 +0200

    Return back to use the static css since the sass isn't needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { TextField } from '@rmwc/textfield';
-import 'material-components-web/material-components-web.scss'
+import 'material-components-web/dist/material-components-web.css'
 
 class App extends Component {
   render() {
