e4da201262721f77e308115df55ed88d70429bd9
Almog Hamdani <almog@Users-MacBook-Air.local>
2019-01-07 14:29:21 +0200

    Add a global theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import { TextField } from '@rmwc/textfield';
 import { Button } from '@rmwc/button';
+import { ThemeProvider, Theme } from '@rmwc/theme';
 import 'material-components-web/dist/material-components-web.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <ThemeProvider className="App" options={{ primary: "blue" }} style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <div className="login-div" style={{ width: "350px", display: "flex", flexDirection: "column" }} >
           <TextField outlined label="E-Mail" />
           <TextField outlined label="Password" type="password" style={{ marginTop: "18px" }} />
           <TextField outlined label="Server IP" style={{ marginTop: "18px" }} />
           <Button raised style={{ marginTop: "18px" }}>Login</Button>
         </div>
-      </div>
+      </ThemeProvider>
     );
   }
 }
