591f38f8bfc9437a59c7a58490c1496f696ec38a
Almog Hamdani <almog@Users-MacBook-Air.local>
2019-01-07 14:07:50 +0200

    Add a margin between the two textfields

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App" style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <div className="login-div" style={{ width: "350px", display: "flex", flexDirection: "column" }} >
           <TextField outlined label="E-Mail" />
-          <TextField outlined label="Password" type="password" />
+          <TextField outlined label="Password" type="password" style={{ marginTop: "18px" }} />
         </div>
       </div>
     );
