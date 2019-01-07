e4717fa5be5130f789f7e11f32d127eeba241fdd
Almog Hamdani <almog@Users-MacBook-Air.local>
2019-01-07 14:04:25 +0200

    Center the children of the main div

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import 'material-components-web/dist/material-components-web.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
         <div className="login-div" style={{ width: "350px", display: "flex", flexDirection: "column" }} >
           <TextField outlined label="E-Mail" />
           <TextField outlined label="Password" type="password" />
