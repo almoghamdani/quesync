7fc062c0cc5a70783b9f3a3e962ce95eaf479cd3
Almog Hamdani <almogh52@gmail.com>
2019-01-07 22:41:54 +0200

    Simple example of sending data to the login event

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+const electron = window.require("electron")
+
+electron.ipcRenderer.send("login-event", "sdsd");
 
 ReactDOM.render(<App />, document.getElementById('root'));
