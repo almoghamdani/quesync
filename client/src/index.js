6af640b7c5942fb31cc51a957a792ce3cfdb73fe
Almog Hamdani <almogh52@gmail.com>
2019-01-07 21:18:25 +0200

    Remove the example usage of the events

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-const electron = window.require("electron")
-
-electron.ipcRenderer.send("login-event", "sdsd");
 
 ReactDOM.render(<App />, document.getElementById('root'));
