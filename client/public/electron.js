5c04511a7194b16efaa3a6407f82cc1739d914b1
Almog Hamdani <almogh52@gmail.com>
2019-01-07 22:41:05 +0200

    Clean the electron main code + Add an event for logging in

--- client/public/electron.js
+++ client/public/electron.js
@@ -1,32 +1,48 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
 
 const path = require('path');
-const url = require('url');
 const isDev = require('electron-is-dev');
 
 const quesync = require('./backend.node');
-var client = new quesync.Client();
-client.connect("127.0.0.1");
 
+// Set the main window as a global var
 let mainWindow;
 
+// Create a new backend client
+var client = new quesync.Client();
+
 function createWindow() {
+  // Create a new browser window with a fixed initial size
   mainWindow = new BrowserWindow({width: 900, height: 680});
+
+  // Load the dev url if electron ran on dev or load the static html file when electron is running in production
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+
+  // On the close event release the window from the memory
   mainWindow.on('closed', () => mainWindow = null);
 }
 
+ipcMain.on('login-event', (event, arg) => {
+  console.log("IDK")
+})
+
+// When the electron app is ready, create the browser window
 app.on('ready', createWindow);
 
+// If all the windows are closed, on windows, quit app
 app.on('window-all-closed', () => {
+  // Shut down app on windows
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+// If the app activated and the main window isn't created, create the window
 app.on('activate', () => {
+  // If the main window is null, create it
   if (mainWindow === null) {
     createWindow();
   }
