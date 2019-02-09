ee790789a4e60f9b4ae3914d52faf318e65792c3
Almog Hamdani <almogh52@gmail.com>
2019-02-09 12:08:42 +0200

    Create a testing client

---
+++ server/testing/client.py
@@ -0,0 +1,9 @@
+import socket
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 61110)
+print('connecting to {} port {}'.format(*server_address))
+sock.connect(server_address)
