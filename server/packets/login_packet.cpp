e7ee2872dfd471e5ab4854971660e8a95c31c64a
Almog Hamdani <almogh52@gmail.com>
2019-02-09 23:30:59 +0200

    Add an empty login packet class

---
+++ server/packets/login_packet.cpp
@@ -0,0 +1,11 @@
+#include "login_packet.h"
+
+void LoginPacket::decode (std::string packet)
+{
+
+}
+
+std::string LoginPacket::encode()
+{
+    return std::string("");
+}
---
+++ server/packets/login_packet.h
@@ -0,0 +1,12 @@
+#include "packet.h"
+
+class LoginPacket : public Packet
+{
+public:
+    virtual std::string encode();
+    virtual void decode (std::string packet);
+
+private:
+    std::string _email;
+    std::string _password;
+}
