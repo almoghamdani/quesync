528b0fddeb7c8474b78c41a127af3e3b17d4a121
Almog Hamdani <almogh52@gmail.com>
2019-02-09 23:51:04 +0200

    Implement the decode function in the login packet

--- server/packets/login_packet.cpp
+++ server/packets/login_packet.cpp
@@ -1,8 +1,14 @@
 #include "login_packet.h"
+#include "..\utils.h"
 
 void LoginPacket::decode (std::string packet)
 {
+    // Split the packet
+    std::vector<std::string> params = Utils::Split(packet, PACKET_DELIMETER);
 
+    // Set the email and password from the params
+    _email = params[1];
+    _password = params[2];
 }
 
 std::string LoginPacket::encode()
