65157e5a9002efb06ac13bf82d52af44d9188bc9
Almog Hamdani <almogh52@gmail.com>
2019-02-09 12:21:08 +0200

    Debug a few of the steps of initializing the server

--- server/main.cpp
+++ server/main.cpp
@@ -7,12 +7,12 @@ int main()
     asio::io_context io_context;
     Quesync *server;
 
-    std::cout << "Starting server.." << std::endl;
-
     try {
+        std::cout << "Initializing Quesync server.." << std::endl;
         // Create the Quesync server
         server = new Quesync(io_context);
 
+        std::cout << "Starting server.." << std::endl;
         // Start the server
         server->start();
 
--- server/quesync.cpp
+++ server/quesync.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "quesync.h"
 #include "session.h"
 
@@ -7,6 +9,8 @@ Quesync::Quesync(asio::io_context& io_context) : _acceptor(io_context, tcp::endp
 
 void Quesync::start()
 {
+    std::cout << "Listening for TCP connections.." << std::endl;
+
     // Start acception requests
     acceptClient();
 }
@@ -20,7 +24,14 @@ void Quesync::acceptClient()
             // If no error occurred during the connection to the client start a session with it
             if (!ec)
             {
+                // Print the client ip and port
+                std::cout << "Client connected from " << socket.remote_endpoint().address().to_string() << ":" << (int)socket.remote_endpoint().port() << std::endl;
+
+                // Create a shared session for the client socket
                 std::make_shared<Session>(std::move(socket), this)->start();
+            } else {
+                // Print error
+                std::cout << "An error occurred: " << ec << std::endl;
             }
 
             // Accept the next client
