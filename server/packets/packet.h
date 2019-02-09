3b7b1f968c8c3255e96f0480a9fc8ab58f828cd6
Almog Hamdani <almogh52@gmail.com>
2019-02-10 00:26:35 +0200

    Remove packet type member

--- server/packets/packet.h
+++ server/packets/packet.h
@@ -10,15 +10,7 @@
 class Packet
 {
 public:
-    PacketType getType() const
-    {
-        return _type;
-    }
-
     virtual std::string encode() = 0;
     virtual void decode (std::string packet) = 0;
     virtual Packet *handle (Quesync *server) = 0;
-
-protected:
-    PacketType _type;
 };
