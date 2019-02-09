51d1eac4c8cb5c7e693ec464904003f1dc25e3e8
Almog Hamdani <almogh52@gmail.com>
2019-02-10 00:25:48 +0200

    Add an abstract function that handles the client's request

--- server/packets/packet.h
+++ server/packets/packet.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 
+#include "..\quesync.h"
 #include "packet_types.h"
 
 #define PACKET_IDENTIFIER "QUESYNC"
@@ -16,6 +17,7 @@ public:
 
     virtual std::string encode() = 0;
     virtual void decode (std::string packet) = 0;
+    virtual Packet *handle (Quesync *server) = 0;
 
 protected:
     PacketType _type;
