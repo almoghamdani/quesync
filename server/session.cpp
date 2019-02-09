518654d34419c9ef3f6a4109f31ccaddb07691dd
Almog Hamdani <almogh52@gmail.com>
2019-02-09 23:19:15 +0200

    Add a recv function from the client

--- server/session.cpp
+++ server/session.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "session.h"
 
 Session::Session(tcp::socket socket, Quesync *server) : 
@@ -8,5 +10,25 @@ Session::Session(tcp::socket socket, Quesync *server) :
 
 void Session::start()
 {
+    // Start receiving from the client
+    recv();
+}
 
+void Session::recv()
+{
+    auto self(shared_from_this());
+
+    // Get a request from the user
+    _socket.async_read_some(asio::buffer(_data, MAX_DATA_LEN),
+        [this, self](std::error_code ec, std::size_t length)
+        {
+            // If no error occurred, parse the request
+            if (!ec)
+            {
+                std::cout << _data << std::endl;
+            } else {
+                // Print error
+                std::cout << "An error occurred: " << ec << std::endl;
+            }
+        });
 }
--- server/session.h
+++ server/session.h
@@ -4,6 +4,8 @@
 
 #include "quesync.h"
 
+#define MAX_DATA_LEN 1000
+
 using asio::ip::tcp;
 
 class Session : public std::enable_shared_from_this<Session>
@@ -12,8 +14,10 @@ public:
     Session(tcp::socket socket, Quesync *server);
 
     void start();
+    void recv();
 
 private:
     tcp::socket _socket;
     Quesync *_server;
+    char _data[MAX_DATA_LEN];
 };
