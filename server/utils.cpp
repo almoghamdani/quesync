5869015e7d45b61e156fcbefdc60603ca482b329
Almog Hamdani <almogh52@gmail.com>
2019-02-10 00:15:20 +0200

    Implement a function to parse the packet

--- server/utils.cpp
+++ server/utils.cpp
@@ -1,6 +1,43 @@
 #include <sstream>
 
 #include "utils.h"
+#include "packets\login_packet.h"
+
+Packet *Utils::ParsePacket(std::string packet)
+{
+    Packet *p = nullptr;
+    PacketType packet_type;
+
+    // Check if the packet starts with the packet identifier, if it isn't return null
+    if (packet.substr(0, strlen(PACKET_IDENTIFIER)) != PACKET_IDENTIFIER)
+    {
+        return nullptr;
+    }
+
+    // Remove the packet identifier from the packet str
+    packet = packet.substr(strlen(PACKET_IDENTIFIER) + 1, packet.length);
+
+    // Get the packet type
+    packet_type = GetPacketType(packet);
+
+    // Remove the packet type from the packet str
+    packet = packet.substr(4, packet.length);
+
+    // By the packet type create the correct packet type
+    switch (packet_type)
+    {
+    case LOGIN_PACKET:
+        p = new LoginPacket();
+    }
+
+    // If the type was found, decode the packet
+    if (p != nullptr)
+    {
+        p->decode(packet);
+    }
+
+    return p;
+}
 
 PacketType Utils::GetPacketType(std::string packet)
 {
--- server/utils.h
+++ server/utils.h
@@ -2,11 +2,13 @@
 #include <string>
 #include <vector>
 
+#include "packets\packet.h"
 #include "packets\packet_types.h"
 
 class Utils
 {
 public:
+    static Packet *ParsePacket(std::string packet);
     static PacketType GetPacketType(std::string packet);
     static std::vector<std::string> Split(const std::string& s, char delimiter);
 };
