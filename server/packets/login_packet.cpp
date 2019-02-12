87337d1a98acd348c5c65ca186282a1a0a121ff8
Almog Hamdani <almogh52@gmail.com>
2019-02-12 15:07:47 +0200

    Implememt the handle function for the login packet

--- server/packets/login_packet.cpp
+++ server/packets/login_packet.cpp
@@ -1,4 +1,5 @@
 #include "login_packet.h"
+#include "response_packet.h"
 #include "..\utils.h"
 
 void LoginPacket::decode (std::string packet)
@@ -9,6 +10,17 @@ void LoginPacket::decode (std::string packet)
     // Set the email and password from the params
     _email = params[0];
     _password = params[1];
+}
+
+std::string LoginPacket::handle (Quesync *server)
+{
+    ResponePacket responePacket;
+
+    // Create a response packet with authenticated code
+    responePacket = ResponsePacket(AUTHENTICATED_PACKET);
+
+    // Encode the response packet
+    return responsePacket.encode();
 }
 
 std::string LoginPacket::encode()
--- server/packets/login_packet.h
+++ server/packets/login_packet.h
@@ -6,6 +6,7 @@ class LoginPacket : public Packet
 public:
     virtual std::string encode();
     virtual void decode (std::string packet);
+    virtual std::string handle (Quesync *server);
 
 private:
     std::string _email;
