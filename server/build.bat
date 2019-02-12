a6f807745b7c4a8ea5a3f28700081ed9f841c1b6
Almog Hamdani <almogh52@gmail.com>
2019-02-13 00:06:11 +0200

    Clean build script + Remove re-building project every time

--- server/build.bat
+++ server/build.bat
@@ -1,22 +1,12 @@
 SET ARCH=x64
 
-::Re-create the build folder
-IF EXIST build rmdir build /s /q
-mkdir build
-
-:: Copy all needed dlls to build folder
-xcopy /s dlls build
+::If the build folder doesn't exist, create it
+IF NOT EXIST build mkdir build
 
 :: Initialize the visual studio developer command line with x64 if it's not initialized yet
 IF NOT DEFINED DevEnvDir (
     call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
 )
-
-::SET LIB=%~dp0\..\lib;%LIB%
-
-:: Re-compile the server side
-::IF EXIST build\server.exe del build\server.exe
-::cl packets\login_packet.cpp utils.cpp session.cpp quesync.cpp main.cpp -I..\include -DASIO_STANDALONE -D_WIN32_WINNT=0x0501 /std:c++17 /EHsc /Fo"build/" /Fe"build\server.exe"
 
 :: Create the cmake project
 cd build
