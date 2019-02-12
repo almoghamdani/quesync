a7e8fb27484cbc80c4a3a5ae7eb52415a67bcbc5
Almog Hamdani <almogh52@gmail.com>
2019-02-12 15:07:07 +0200

    Update response packet implementation

--- server/packets/response_packet.h
+++ server/packets/response_packet.h
@@ -1,9 +1,25 @@
 #include "packet.h"
 
+#include <format>
+
 class ResponsePacket : public Packet
 {
 public:
     ResponsePacket(PacketType type) : _type(type)
     {
-    }
+    };
+    
+    virtual std::string encode()
+    {
+        // Format the response type in the packet
+        return fmt::format("{1}{0}{2}", PACKET_DELIMETER, PACKET_IDENTIFIER, _type);
+    };
+
+    virtual void decode (std::string packet)
+    {
+    };
+
+    virtual std::string handle (Quesync *server)
+    {
+    };
 };
