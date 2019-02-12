c4be9b8a2677c04b45a3eaa8a1cfc06f97b6f078
Almog Hamdani <almogh52@gmail.com>
2019-02-12 23:57:31 +0200

    Convert login packet to header-only

--- server/packets/login_packet.h
+++ server/packets/login_packet.h
@@ -1,12 +1,32 @@
 #pragma once
 #include "packet.h"
 
+#include "response_packet.h"
+#include "..\utils.h"
+
 class LoginPacket : public Packet
 {
 public:
-    virtual std::string encode();
-    virtual void decode (std::string packet);
-    virtual std::string handle (Quesync *server);
+    virtual std::string encode()
+    {
+        return std::string("");
+    };
+
+    virtual void decode (std::string packet)
+    {
+        // Split the packet
+        std::vector<std::string> params = Utils::Split(packet, PACKET_DELIMETER);
+
+        // Set the email and password from the params
+        _email = params[0];
+        _password = params[1];
+    };
+
+    virtual std::string handle (Quesync *server)
+    {
+        // Encode the response packet
+        return ResponsePacket(AUTHENTICATED_PACKET).encode();
+    };
 
 private:
     std::string _email;
