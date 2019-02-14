3a551d8326970facaf6ef5e5ccbd63cf49565d3f
Almog Hamdani <almogh52@gmail.com>
2019-02-14 18:43:53 +0200

    Add a class for quesync excpetions

---
+++ server/quesync_exception.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <exception>
+#include <string>
+
+#include "errors.h"
+
+#define MAX_ERROR_MSG_LEN 100
+
+class QuesyncException : public std::exception
+{
+public:
+    QuesyncException(QuesyncError ec)
+    {
+        // Set the error message by the error code
+        sprintf(_errorMsg, "An error occurred, error code: %d", ec);
+    };
+
+    virtual const char* what() const throw (){
+       return _errorMsg;
+    }
+
+private:
+    char _errorMsg[MAX_ERROR_MSG_LEN];
+}
