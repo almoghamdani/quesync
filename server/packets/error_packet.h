95d59cd9f30bb2808687f79fecb39c41d66ce082
Almog Hamdani <almogh52@gmail.com>
2019-02-15 00:15:13 +0200

    Add leading zeros to the error code in the error packet data

--- server/packets/error_packet.h
+++ server/packets/error_packet.h
@@ -2,10 +2,14 @@
 #include "response_packet.h"
 #include "../errors.h"
 
+#define ERROR_CODE_LEN 3
+
 class ErrorPacket : public ResponsePacket
 {
 public:
-    ErrorPacket(QuesyncError ec) : _ec(ec), ResponsePacket(QUESYNC_ERROR, std::to_string(ec))
+    ErrorPacket(QuesyncError ec) : _ec(ec), 
+        ResponsePacket(QUESYNC_ERROR, 
+                    std::string(ERROR_CODE_LEN - std::to_string(ec).length(), '0') + std::to_string(ec)) // Add leading zeros to the error code
     {
     };
 
