df9f61232a2e208f38fe9f3267794fed53b34650
Almog Hamdani <almogh52@gmail.com>
2019-02-14 18:36:53 +0200

    Change User ID field name

--- server/user.h
+++ server/user.h
@@ -4,18 +4,18 @@
 class User : public SerializedObject
 {
 public:
-    User(std::string username, std::string email, std::string nickname, std::string user_id)
+    User(std::string username, std::string email, std::string nickname, std::string id)
     {
         // Save all fields in the json type
         _json["username"] = username;
         _json["email"] = email;
         _json["nickname"] = nickname;
-        _json["user_id"] = user_id;
+        _json["id"] = id;
     };
 
     // Get functions for all the user's fields
     GET_FUNCTION(username, std::string)
     GET_FUNCTION(email, std::string)
     GET_FUNCTION(nickname, std::string)
-    GET_FUNCTION(user_id, std::string)
+    GET_FUNCTION(id, std::string)
 }
