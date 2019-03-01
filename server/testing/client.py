1ee2635e8b441792db0959fb4ecd8494fc809892
Almog Hamdani <almogh52@gmail.com>
2019-03-01 21:48:09 +0200

    Update the client tester

--- server/testing/client.py
+++ server/testing/client.py
@@ -1,4 +1,6 @@
 import socket
+import random
+import string
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,6 +10,12 @@ server_address = ('localhost', 61110)
 print('connecting to {} port {}'.format(*server_address))
 sock.connect(server_address)
 
-sock.send(b'QUESYNC|002|{ "username": "sdd", "email": "almogh@gmail.com", "nickname": "sdsd", "password": "123456789" }|')
+idk = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
+#sock.send(('QUESYNC|002|{ "username": "almogh23", "email": "' + idk + '@gmail.com", "nickname": "sdsd", "password": "123456789" }|').encode())
+#sock.send(b'QUESYNC|003|{ "nickname": "s", "tag": 9591 }|')
+sock.send(b'QUESYNC|001|{ "username": "almogh52", "password": "123456789" }|')
+print(sock.recv(512).decode())
+
+sock.send(b'QUESYNC|004|{ "recipient_id": "530e9407-7c-4be4-9cc6-70ab38e6310c" }|')
 
 print(sock.recv(512).decode())
