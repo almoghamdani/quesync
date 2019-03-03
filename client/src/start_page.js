1ad84c6ecc8d26312d005e6d5fd1311bcddaf217
Almog Hamdani <almogh52@gmail.com>
2019-03-03 13:48:58 +0200

    Re-designed the login and made it into the start page

--- client/src/start_page.js
+++ client/src/start_page.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import './start_page.css';
+import { Typography } from '@rmwc/typography';
 import { TextField } from '@rmwc/textfield';
 import { Button, ButtonIcon } from '@rmwc/button';
 import { ThemeProvider } from '@rmwc/theme';
 import { CircularProgress } from '@rmwc/circular-progress';
-import 'material-components-web/dist/material-components-web.css'
+import { Elevation } from '@rmwc/elevation';
+
 import '@rmwc/circular-progress/circular-progress.css'
 
 const electron = window.require("electron")
 
-class App extends Component {
+class StartPage extends Component {
   /**
    * This function is the constructor of the App class
    * 
@@ -24,49 +26,28 @@ class App extends Component {
     // Set initial state of component
     this.state = { 
       connecting: false,
-      usernameError: false,
-      passwordError: false,
+
       serverIPError: false
     }
 
     // Make 'this' work in the event funcion
-    this.loginClicked = this.loginClicked.bind(this);
+    this.connectBtnClicked = this.connectBtnClicked.bind(this);
   }
 
   /**
-   * This function is an event function that will be called when the login button was clicked.
+   * This function is an event function that will be called when the connect button was clicked.
    * 
    * @return None
    */
-  loginClicked()
+  connectBtnClicked()
   {
     // Reset errors
     this.setState({
-        usernameError: false,
-        passwordError: false,
         serverIPError: false
     });
 
-    // If username empty, set it as invalid
-    if (this.refs.username.input_.value.length == 0)
-    {
-        // Set it as invalid
-        this.setState({
-            usernameError: true
-        })
-        return;
-    }
-    // If password empty, set it as invalid
-    else if (this.refs.password.input_.value.length == 0)
-    {
-        // Set it as invalid
-        this.setState({
-            passwordError: true
-        })
-        return;
-    }
     // If server IP empty or not matching the validation pattern, set it as invalid
-    else if (this.refs.serverIP.input_.value.length == 0 || 
+    if (this.refs.serverIP.input_.value.length == 0 || 
         !/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(this.refs.serverIP.input_.value))
     {
         // Set it as invalid
@@ -93,21 +74,16 @@ class App extends Component {
 
         // Save client in window
         window.client = electron.remote.getGlobal("client");
-
-        // Try to login with the username and password
-        var {error, user} = window.client.login(this.refs.username.input_.value, this.refs.password.input_.value);
-
-        console.log(user)
     }
   }
 
   render() {
     return (
-      <ThemeProvider className="App" options={{ primary: "#ff3d00", secondary:"white" }} style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <div className="login-div" style={{ width: "350px", display: "flex", flexDirection: "column" }} >
+      <ThemeProvider className="App" options={{ primary: "#00b0ff", secondary:"#e0e0e0" }} style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        {/*<div className="login-div" style={{ width: "350px", display: "flex", flexDirection: "column" }} >
           <TextField invalid={this.state.usernameError} outlined label="Username" ref="username" />
           <TextField invalid={this.state.passwordError} outlined label="Password" ref="password" type="password" style={{ marginTop: "18px" }} />
-          <TextField invalid={this.state.serverIPError} outlined label="Server IP" ref="serverIP" style={{ marginTop: "18px" }} pattern="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$" />
+          
           <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
             <Button raised style={{ marginTop: "18px", width: "160px" }} onClick={this.loginClicked}>
               {
@@ -117,10 +93,23 @@ class App extends Component {
             </Button>
             <Button raised style={{ marginTop: "18px", width: "160px", backgroundColor: "#00b0ff" }}>Register</Button>
           </div>
-        </div>
+        </div>*/}
+
+        <Elevation style={{ width: "25rem", height: "20rem", background: "var(--mdc-theme-secondary)" }} z="8">
+            <div className="connect-form" style={{ minHeight: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                <Typography use="headline2" style={{ color: "var(--mdc-theme-primary)" }}>Quesync</Typography>
+                <TextField invalid={this.state.serverIPError} outlined label="Server IP" ref="serverIP" style={{ marginTop: "50px", width: "300px" }} pattern="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$" />
+                <Button raised style={{ marginTop: "25px", width: "300px" }} theme={['secondary']} onClick={this.connectBtnClicked}>
+                {
+                    this.state.connecting ? <ButtonIcon icon={<CircularProgress theme="secondary" />}/> : null
+                }
+                Connect
+                </Button>
+            </div>
+        </Elevation>
       </ThemeProvider>
     );
   }
 }
 
-export default App;
+export default StartPage;
