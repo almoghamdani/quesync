1d736377964eea4f1b4ac478acaa12aea463c1a2
Almog Hamdani <almogh52@gmail.com>
2019-03-07 00:09:46 +0200

    Update the design for the start menu + Remove background gradient animation

--- client/src/start_page.js
+++ client/src/start_page.js
@@ -39,19 +39,6 @@ class StartPage extends Component {
     this.loginBtnClicked = this.loginBtnClicked.bind(this);
   }
 
-  componentDidMount()
-  {
-      // Add an animation for the background that will run in loops
-      anime({
-          targets: "body",
-          direction: 'alternate',
-          loop: true,
-          easing: 'easeInOutQuad',
-          backgroundPosition: 100,
-          duration: 7500
-      })
-  }
-
   /**
    * This function is an event function that will be called when the connect button was clicked.
    * 
@@ -175,20 +162,61 @@ class StartPage extends Component {
         <ParticlesNoUpdate params={{
             "particles": {
                 "number": {
-                    "value": 50, 
+                    "value": 160,
                     "density": {
                         "enable": true,
-                        "value_area": 500
+                        "value_area": 800
                     }
+                },
+                "color": {
+                    "value": "#ffffff"
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    },
+                    "image": {
+                        "src": "img/github.svg",
+                        "width": 100,
+                        "height": 100
+                    }
+                },
+                "opacity": {
+                    "value": 1,
+                    "random": true,
+                    "anim": {
+                        "enable": true,
+                        "speed": 1,
+                        "opacity_min": 0,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 4,
+                        "size_min": 0.3,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": false,
                 },
                 "move": {
                     "enable": true,
-                    "speed": 1.8,
-                    "direction": "none",
+                    "speed": 0.6,
+                    "direction": "top",
                     "random": true,
                     "straight": false,
                     "out_mode": "out",
-                    "bounce": false
+                    "bounce": true,
                 }
             },
             "interactivity": {
@@ -196,16 +224,16 @@ class StartPage extends Component {
                 "events": {
                     "onhover": {
                         "enable": true,
-                        "mode": "grab"
-                    },
-                    "resize": true
+                        "mode": "bubble"
+                    }
                 },
                 "modes": {
-                    "grab": {
-                        "distance": 180,
-                        "line_linked": {
-                            "opacity": 1
-                        }
+                    "bubble": {
+                        "distance": 75,
+                        "size": 0,
+                        "duration": 2.2,
+                        "opacity": 0,
+                        "speed": 3
                     }
                 }
             }
