f2fac1e859c256142406271c2aeb71d2998ca34d
Almog Hamdani <almogh52@gmail.com>
2019-03-07 14:48:39 +0200

    Finish the design of the drag zone + Add buttons for closing, maximizing and minimizing

--- client/src/layout.js
+++ client/src/layout.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import './layout.scss';
+import { IconButton } from '@rmwc/icon-button';
+
+const electron = window.require("electron")
+const BrowserWindow = electron.remote.BrowserWindow
+const currentWindow = BrowserWindow.getFocusedWindow()
 
 class Layout extends Component
 {
+    close()
+    {
+        // Close the current window
+        currentWindow.close()
+
+        // Blur active element
+        document.activeElement.blur();
+    }
+
+    maximize()
+    {
+        // Maximize or unmaximize the current window by the current maximized state
+        currentWindow.isMaximized() ? currentWindow.unmaximize() : currentWindow.maximize()
+
+        // Blur active element
+        document.activeElement.blur();
+    }
+
+    minimize()
+    {
+        // Minimize the current window
+        currentWindow.minimize()
+
+        // Blur active element
+        document.activeElement.blur();
+    }
+
     render()
     {
         return (
-            <div className="quesync-layout" style={{ maxHeight: "100%", maxWidth: "100%" }}>
-                <div className="quesync-drag-zone" style={{ position: "absolute", top:"0", left: "0", height: "20px", minWidth: "100%", webkitAppRegion: "drag", background: "black", zIndex: "100" }}>
+            <div className="quesync-layout">
+                <div className="quesync-drag-holder">
+                    <div className="quesync-drag-zone" />
+                    <IconButton className="quesync-drag-zone-button" icon="minimize" onClick={this.minimize}/>
+                    <IconButton className="quesync-drag-zone-button" icon="fullscreen" onClick={this.maximize}/>
+                    <IconButton className="quesync-drag-zone-button" icon="close" onClick={this.close}/>
                 </div>
                 {this.props.child}
             </div>
