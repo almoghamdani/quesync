2924c3b3ff2d13fa6fd41925d387205a968021ac
Almog Hamdani <almogh52@gmail.com>
2019-04-13 17:03:25 +0300

    Add actions for starting and finished authentication

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,20 @@
 export function signIn(client, username, password) {
-    return {
-        type: "USER_LOGIN",
-        payload: client.login(username, password)
-    }
+	return {
+		type: "USER_LOGIN",
+		payload: client.login(username, password)
+	};
+}
+
+export function startAuth() {
+	return {
+		type: "AUTH_STARTED",
+		payload: null
+	};
+}
+
+export function finishAuth() {
+	return {
+		type: "AUTH_FINISHED",
+		payload: null
+	};
 }
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,24 +1,31 @@
 export default function reducer(
-  state = {
-    user: null,
-    authError: null
-  },
-  action
+	state = {
+		authenticating: false,
+		user: null,
+		authError: null
+	},
+	action
 ) {
-  switch (action.type) {
-    case "USER_SIGNUP_PENDING":
-    case "USER_LOGIN_PENDING":
-      return { ...state, authError: null };
+	switch (action.type) {
+		case "USER_SIGNUP_PENDING":
+		case "USER_LOGIN_PENDING":
+			return { ...state, authError: null };
 
-    case "USER_SIGNUP_REJECTED":
-    case "USER_LOGIN_REJECTED":
-      return { ...state, authError: action.payload.error };
+		case "USER_SIGNUP_REJECTED":
+		case "USER_LOGIN_REJECTED":
+			return { ...state, authError: action.payload.error };
 
-    case "USER_SIGNUP_FULFILLED":
-    case "USER_LOGIN_FULFILLED":
-      return { ...state, user: action.payload.user };
+		case "USER_SIGNUP_FULFILLED":
+		case "USER_LOGIN_FULFILLED":
+			return { ...state, user: action.payload.user };
 
-    default:
-      return state;
-  }
+		case "AUTH_STARTED":
+			return { ...state, authenticating: true };
+
+		case "AUTH_FINISHED":
+			return { ...state, authenticating: false };
+
+		default:
+			return state;
+	}
 }
