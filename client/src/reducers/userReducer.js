94ec9eef290f2921aa6665666570213d31caf3c1
Almog Hamdani <almogh52@gmail.com>
2019-04-13 16:55:11 +0300

    Remove indications for signing up and logging in since they are not needed

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,30 +1,22 @@
 export default function reducer(
   state = {
-    loggingIn: false,
-    signingUp: false,
     user: null,
     authError: null
   },
   action
 ) {
   switch (action.type) {
-    case "USER_LOGIN_PENDING":
-      return { ...state, loggingIn: true, authError: null };
-
-    case "USER_LOGIN_REJECTED":
-      return { ...state, loggingIn: false, authError: action.payload.error };
-
-    case "USER_LOGIN_FULFILLED":
-      return { ...state, loggingIn: false, user: action.payload.user };
-
     case "USER_SIGNUP_PENDING":
-      return { ...state, signingUp: true, authError: null };
+    case "USER_LOGIN_PENDING":
+      return { ...state, authError: null };
 
     case "USER_SIGNUP_REJECTED":
-      return { ...state, signingUp: false, authError: action.payload.error };
+    case "USER_LOGIN_REJECTED":
+      return { ...state, authError: action.payload.error };
 
     case "USER_SIGNUP_FULFILLED":
-      return { ...state, signingUp: false, user: action.payload.user };
+    case "USER_LOGIN_FULFILLED":
+      return { ...state, user: action.payload.user };
 
     default:
       return state;
