059eb2754815d172b04640f75dca39af89df57f4
Almog Hamdani <almogh52@gmail.com>
2019-04-14 22:52:13 +0300

    Fix wrong user register action names

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -7,15 +7,15 @@ export default function reducer(
 	action
 ) {
 	switch (action.type) {
-		case "USER_SIGNUP_PENDING":
+		case "USER_REGISTER_PENDING":
 		case "USER_LOGIN_PENDING":
 			return { ...state, authError: null };
 
-		case "USER_SIGNUP_REJECTED":
+		case "USER_REGISTER_REJECTED":
 		case "USER_LOGIN_REJECTED":
 			return { ...state, authError: action.payload.error };
 
-		case "USER_SIGNUP_FULFILLED":
+		case "USER_REGISTER_FULFILLED":
 		case "USER_LOGIN_FULFILLED":
 			return { ...state, user: action.payload.user };
 
