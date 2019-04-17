50243c66b970849a33e7ca240edfa3b0536a9518
Almog Hamdani <almog@almogs-imac.home>
2019-04-18 00:22:59 +0300

    Design a simple app layout

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
 
+import {
+	TopAppBar,
+	TopAppBarRow,
+	TopAppBarSection,
+	TopAppBarNavigationIcon,
+	TopAppBarTitle,
+	TopAppBarFixedAdjust
+} from "@rmwc/top-app-bar";
+import { ThemeProvider } from "@rmwc/theme";
+
 class App extends Component {
 	render() {
-		return <div className="quesync-app" style={{ ...this.props.style, width: "100vw", height: "100vh", background: "blue", position: "absolute", top: 0, left: 0 }} />;
+		return (
+			<ThemeProvider
+				className="quesync-app"
+				style={{
+					...this.props.style,
+					width: "100vw",
+					height: "100vh",
+					background: "#424242",
+					position: "absolute",
+					top: 0,
+					left: 0
+				}}
+				options={{ primary: "#212121", secondary: "#e0e0e0" }}
+			>
+				<div
+					className="quesync-seperator"
+					style={{ width: "100vw", height: "30px", background: "black" }}
+				/>
+				<TopAppBar>
+					<TopAppBarRow>
+						<TopAppBarSection alignStart>
+							<TopAppBarNavigationIcon icon="menu" />
+							<TopAppBarTitle>All Features</TopAppBarTitle>
+						</TopAppBarSection>
+					</TopAppBarRow>
+				</TopAppBar>
+				<TopAppBarFixedAdjust />
+			</ThemeProvider>
+		);
 	}
 }
 
