271f6a2f7d6e69bc24ec687a7698f3e670bf7dee
Almog Hamdani <almogh52@gmail.com>
2019-04-24 01:24:22 +0300

    Prettify

--- client/src/reducers/clientReducer.js
+++ client/src/reducers/clientReducer.js
@@ -1,15 +1,14 @@
 export default function reducer(
-    state = {
-      client: null
-    },
-    action
-  ) {
-    switch (action.type) {
-      case "CLIENT_OBJECT":
-        return {...state, client: action.payload}
-  
-      default:
-        return state;
-    }
-  }
-  
+	state = {
+		client: null
+	},
+	action
+) {
+	switch (action.type) {
+		case "CLIENT_OBJECT":
+			return { ...state, client: action.payload };
+
+		default:
+			return state;
+	}
+}
