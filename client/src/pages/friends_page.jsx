80841287e1f9afc4ed2051e74489343b01e3d7a8
Almog Hamdani <almog@almogs-imac.home>
2019-04-26 02:06:21 +0300

    Implmemnt a simple layout for the friends page

--- client/src/pages/friends_page.jsx
+++ client/src/pages/friends_page.jsx
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { Scrollbars } from "react-custom-scrollbars";
+
+import { Drawer, DrawerContent, DrawerAppContent } from "@rmwc/drawer";
+import { List, ListItem } from "@rmwc/list";
 
 import "./page.scss";
 
 class FriendsPage extends Component {
 	render() {
-		return <div className="quesync-page quesync-friends-page" />;
+		const friends = this.props.user.friends.map(
+			friendId => this.props.profiles[friendId].nickname
+		);
+
+		return (
+			<div className="quesync-page quesync-friends-page">
+				<Drawer className="quesync-drawer">
+					<DrawerContent>
+						<List>
+							{friends.map((friend, idx) => (
+								<ListItem key={idx}>{friend}</ListItem>
+							))}
+						</List>
+					</DrawerContent>
+				</Drawer>
+				<DrawerAppContent className="quesync-drawer-content">
+					<Scrollbars>
+						<div className="quesync-app-content" />
+					</Scrollbars>
+				</DrawerAppContent>
+			</div>
+		);
 	}
 }
 
-export default FriendsPage;
+export default connect(state => ({
+	user: state.auth.user,
+	profiles: state.users.profiles
+}))(FriendsPage);
