6d37e7bf4c1491a34f72d8153804521871e60a1e
Almog Hamdani <almogh52@gmail.com>
2019-06-19 21:19:41 +0300

    Add missing onClick event pass through

--- client/src/components/friend_request_item.jsx
+++ client/src/components/friend_request_item.jsx
@@ -10,6 +10,7 @@ class FriendRequestItem extends Component {
 			<DrawerItem
 				className="quesync-friend-request-item"
 				key={this.props.key}
+				onClick={this.props.onClick}
 				avatarUrl={this.props.friendAvatarUrl}
 				itemName={this.props.friendName}
 				itemInfo={<Moment unix fromNow interval={10000} date={this.props.sentAt}/>}
