39700641471717e3e330001c75c6c4835436ed8f
Almog Hamdani <almogh52@gmail.com>
2019-07-06 23:56:36 +0300

    Implement the handler for the friendship status event in the client

--- client/src/event_handler.js
+++ client/src/event_handler.js
@@ -13,7 +13,15 @@ class EventHandler {
 	register = client => {
 		client.registerEventHandler("ping", this.pingEvent);
 		client.registerEventHandler("friend-request", this.friendRequestEvent);
+		client.registerEventHandler("friendship-status", this.friendshipStatusEvent);
 		client.registerEventHandler("message", this.messageEvent);
+	};
+
+	pingEvent = async event => {
+		const pingValue = event.ping;
+
+		// Add the ping value to the list of pings
+		await store.dispatch(addPingValue(pingValue));
 	};
 
 	friendRequestEvent = async event => {
@@ -30,9 +38,7 @@ class EventHandler {
 		await store.dispatch(
 			setUser({
 				...user,
-				friendRequests: user.friendRequests.concat([
-					{ friendId, sentAt, friendType: "requester" }
-				])
+				friendRequests: [...user.friendRequests, { friendId, sentAt, friendType: "requester" }]
 			})
 		);
 
@@ -46,15 +52,84 @@ class EventHandler {
 					title: 'Dismiss'
 				}
 			]
-		})
+		});
 	};
 
-	pingEvent = async event => {
-		const pingValue = event.ping;
+	friendshipStatusEvent = async event => {
+		const state = store.getState();
+		const user = state.user.user;
+		const { friendId, status } = event;
 
-		// Add the ping value to the list of pings
-		await store.dispatch(addPingValue(pingValue));
-	};
+		// Try to find the friend request
+		const friendRequest = user.friendRequests.find(req => req.friendId === friendId);
+
+		// If the friend request has been found
+		if (friendRequest) {
+			// If the friend request has been approved
+			if (status === true) {
+				// Add the friend to the friends list and remove him from the friend requests
+				await store.dispatch(
+					setUser({
+						...user,
+						friendRequests: user.friendRequests.filter(req => req.friendId !== friendId),
+						friends: [...user.friends, friendId]
+					})
+				);
+
+				// Show a notification with the new friend
+				queue.notify({
+					title: <b>Friend Request Approved</b>,
+					body: <span><b>{store.getState().users.profiles[friendId].nickname}</b> has approved your friend request</span>,
+					icon: "person_add",
+					actions: [
+						{
+							title: 'Dismiss'
+						}
+					]
+				});
+			} else {
+				// Remove the friend from the friend requests
+				await store.dispatch(
+					setUser({
+						...user,
+						friendRequests: user.friendRequests.filter(req => req.friendId !== friendId)
+					})
+				);
+
+				// Show a notification with the rejected friend request
+				queue.notify({
+					title: <b>{friendRequest.friendType === "requester" ? "Friend Request Removed" : "Friend Request Rejected"}</b>,
+					body: <span><b>{store.getState().users.profiles[friendId].nickname}</b> has {friendRequest.friendType === "requester" ? "removed his friend request to you" : "rejected your friend request"}</span>,
+					icon: "person_add",
+					actions: [
+						{
+							title: 'Dismiss'
+						}
+					]
+				});
+			}
+		} else if (status === false && user.friends.indexOf(friendId) !== -1) {
+			// Remove the friend from the friends list
+			await store.dispatch(
+				setUser({
+					...user,
+					friendRequests: user.friends.splice(user.friends.indexOf(friendId), 1)
+				})
+			);
+
+			// Show a notification with the rejected friend request
+			queue.notify({
+				title: <b>Friend Removed</b>,
+				body: <span><b>{store.getState().users.profiles[friendId].nickname}</b> has removed you from his friends list</span>,
+				icon: "person",
+				actions: [
+					{
+						title: 'Dismiss'
+					}
+				]
+			});
+		}
+	}
 
 	messageEvent = event => {
 		var message = { ...event.message };
