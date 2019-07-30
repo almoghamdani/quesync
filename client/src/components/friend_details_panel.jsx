b2d33537e2246c84de83d8877de7260af08cc870
Almog Hamdani <almogh52@gmail.com>
2019-07-31 02:00:15 +0300

    Reorganize the friend details panel and add a call button #1 #5

--- client/src/components/friend_details_panel.jsx
+++ client/src/components/friend_details_panel.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Elevation } from "@rmwc/elevation";
 import { Typography } from "@rmwc/typography";
 import { Avatar } from "@rmwc/avatar";
+import { Button } from "@rmwc/button";
 
 import "./friend_details_panel.scss";
 
@@ -10,12 +11,22 @@ class FriendDetailsPanel extends Component {
 	render() {
 		return (
 			<Elevation z={10} className="quesync-friend-details-panel">
-				<Avatar className="quesync-friend-avatar" src={this.props.avatarUrl} />
-				<Typography className="quesync-friend-nickname-tag" use="headline6">
-					<span className="quesync-friend-nickname">{this.props.nickname}</span>
-					<br />
-					<span className="quesync-friend-tag">#{this.props.tag}</span>
-				</Typography>
+				<div className="quesync-friend-details">
+					<Avatar
+						className="quesync-friend-avatar"
+						src={this.props.avatarUrl}
+					/>
+					<Typography className="quesync-friend-nickname-tag" use="headline6">
+						<span className="quesync-friend-nickname">
+							{this.props.nickname}
+						</span>
+						<br />
+						<span className="quesync-friend-tag">#{this.props.tag}</span>
+					</Typography>
+				</div>
+				<div className="quesync-friend-actions">
+					<Button raised icon="call">Call</Button>
+				</div>
 			</Elevation>
 		);
 	}
