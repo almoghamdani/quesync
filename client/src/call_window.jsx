301022daf3dc0b235cd93f12f82052f7374074bb
Almog Hamdani <almogh52@gmail.com>
2019-08-06 21:35:25 +0300

    Create the UI for the call window #1

---
+++ client/src/call_window.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import { ThemeProvider } from "@rmwc/theme";
+import { Elevation } from "@rmwc/elevation";
+import { Typography } from "@rmwc/typography";
+import { Button } from "@rmwc/button";
+
+import "./call_window.scss";
+
+class CallWindow extends Component {
+	render() {
+		return (
+			<ThemeProvider
+				className="quesync-call-window"
+				options={{
+					primary: "#00b0ff",
+					secondary: "#282828",
+					error: "#ff1744",
+					onSurface: "rgba(255,255,255,.87)"
+				}}>
+				<Elevation className="quesync-call-window-content" z={6}>
+					<img
+						className="quesync-call-avatar"
+						src={this.props.avatar}
+						alt="Incoming Call"
+					/>
+					<Typography className="quesync-call-nickname" use="headline5">
+						{this.props.nickname}
+					</Typography>
+					<div className="quesync-call-actions">
+						<Button
+							raised
+							className="quesync-call-action-button quesync-call-accept-button"
+							label="Answer"
+						/>
+						<Button
+							raised
+							danger
+							className="quesync-call-action-button quesync-call-decline-button"
+							label="Reject"
+						/>
+					</div>
+				</Elevation>
+			</ThemeProvider>
+		);
+	}
+}
+
+export default CallWindow;
--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,31 @@ import { Provider } from "react-redux";
 // Must be here to compile the material components SASS first
 import "./index.scss";
 
-import Layout from "./layout.jsx";
-import StartPage from "./start_page.jsx";
+import Layout from "./layout";
+import StartPage from "./start_page";
+import CallWindow from "./call_window";
 
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store";
+
+import queryString from "query-string";
+
+const callingWindow = window.location.search.includes("calling");
 
 const app = document.getElementById("root");
 
-ReactDOM.render(
-	<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<Layout child={<StartPage />} />
-		</PersistGate>
-	</Provider>,
-	app
-);
+if (callingWindow) {
+	ReactDOM.render(
+		<CallWindow {...queryString.parse(window.location.search)} />,
+		app
+	);
+} else {
+	ReactDOM.render(
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				<Layout child={<StartPage />} />
+			</PersistGate>
+		</Provider>,
+		app
+	);
+}
