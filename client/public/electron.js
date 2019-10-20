594aa10e0fed82d79324bd8ffe73da64dfae0a65
Almog Hamdani <almogh52@gmail.com>
2019-10-20 22:16:32 +0300

    fix: remove menu in server ip window

--- client/public/electron.js
+++ client/public/electron.js
@@ -121,6 +121,9 @@ function createServerIPWindow() {
 		show: false
 	});
 
+	// Remove the menu of the Server IP Window
+	serverIPWindow.removeMenu();
+
 	// Load the dev url if electron ran on dev or load the static html file when electron is running in production
 	serverIPWindow.loadURL(
 		isDev
