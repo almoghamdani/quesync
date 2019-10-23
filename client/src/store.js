af87e5d4450c78fa26d81b90ee7fc3c132497ffd
Almog Hamdani <almogh52@gmail.com>
2019-10-23 16:54:14 +0300

    refactor: enable redux-logger only on dev

--- client/src/store.js
+++ client/src/store.js
@@ -1,25 +1,29 @@
 import { applyMiddleware, createStore, compose } from "redux";
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // localStorage
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
 
-import { createLogger } from "redux-logger";
+import logger from "redux-logger";
 import promise from "redux-promise-middleware";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
 import reducers from "./reducers";
 
+const isDev = window.require("electron-is-dev");
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = applyMiddleware(thunk, promise, createLogger());
+const middlewares = isDev
+	? applyMiddleware(thunk, promise, logger)
+	: applyMiddleware(thunk, promise);
 
 const persistConfig = {
-	key: 'root',
+	key: "root",
 	storage,
-	whitelist: ['ui']
-}
+	whitelist: ["ui"]
+};
 
-const persistedReducer = persistReducer(persistConfig, reducers)
+const persistedReducer = persistReducer(persistConfig, reducers);
 
-let store = createStore(persistedReducer, composeEnhancers(middlewares))
-let persistor = persistStore(store)
+let store = createStore(persistedReducer, composeEnhancers(middlewares));
+let persistor = persistStore(store);
 
-export { store, persistor }
+export { store, persistor };
