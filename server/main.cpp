56cf6f1d89f128d7cad4cf5270f0fc5ff4a4cd12
Almog Hamdani <almogh52@gmail.com>
2019-10-24 20:20:09 +0300

    feat: implement server multithreading (#41)
    Resolve #41

--- server/main.cpp
+++ server/main.cpp
@@ -1,43 +1,72 @@
+#include <chrono>
 #include <iostream>
+#include <termcolor/termcolor.hpp>
+#include <thread>
 
 #include "server.h"
 
-#include <termcolor/termcolor.hpp>
+#define NUM_OF_THREADS 6
 
 int main() {
     asio::io_context io_context;
+    std::vector<std::thread> threads;
+
     std::shared_ptr<quesync::server::server> server;
 
-    std::cout << termcolor::green << termcolor::bold << "Quesync Server v0.1.0" << std::endl;
+    std::cout << termcolor::green << termcolor::bold << "Quesync Server v0.1.0"
+              << "\n";
     std::cout << termcolor::green << termcolor::underline << "All Rights Reserved to Almog Hamdani."
-              << std::endl;
-    std::cout << termcolor::reset << std::endl;
+              << "\n";
+    std::cout << termcolor::reset << "\n";
 
     try {
-        std::cout << termcolor::blue << "Initializing Quesync server.." << termcolor::reset
-                  << std::endl;
+        std::cout << termcolor::blue << "Initializing Quesync server.." << termcolor::reset << "\n";
+
         // Create the Quesync server
         server = std::make_shared<quesync::server::server>(io_context);
 
-        std::cout << termcolor::blue << "Starting server.." << termcolor::reset << std::endl;
+        std::cout << termcolor::blue << "Starting threads.." << termcolor::reset << "\n";
+
+        // Start the threads
+        for (int i = 0; i < NUM_OF_THREADS; i++) {
+            std::cout << termcolor::blue << "Starting thread no. " << i + 1 << ".."
+                      << termcolor::reset << "\n";
+
+            threads.push_back(std::thread([&] {
+                while (true) {
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+                    try {
+                        io_context.run();
+                    } catch (std::exception &ex) {
+                        std::cout << termcolor::red << "Exception occurred in thread "
+                                  << std::this_thread::get_id() << " : " << ex.what()
+                                  << termcolor::reset << "\n";
+                    } catch (...) {
+                        std::cout << termcolor::red << "An unknown error has occurred in thread "
+                                  << std::this_thread::get_id() << "!" << termcolor::reset << "\n";
+                    }
+                }
+            }));
+        }
+
+        std::cout << termcolor::blue << "Starting server.." << termcolor::reset << "\n";
+
         // Start the server
         server->start();
 
-        // Run the io handler
-        io_context.run();
+        // Join the threads
+        for (int i = 0; i < threads.size(); i++) {
+            if (threads[i].joinable()) threads[i].join();
+        }
     } catch (std::exception &ex) {
         std::cout << termcolor::red << "Exception occurred: " << ex.what() << termcolor::reset
-                  << std::endl;
+                  << "\n";
     } catch (std::string &ex) {
-        std::cout << termcolor::red << "Exception occurred: " << ex << termcolor::reset
-                  << std::endl;
+        std::cout << termcolor::red << "Exception occurred: " << ex << termcolor::reset << "\n";
     } catch (...) {
-        std::cout << termcolor::red << "An unknown error has occurred!" << termcolor::reset
-                  << std::endl;
+        std::cout << termcolor::red << "An unknown error has occurred!" << termcolor::reset << "\n";
     }
-
-    // Wait for key press
-    system("pause");
 
     return 0;
 }
